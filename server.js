e828dba5dddcf8feb07a079f5f6edfaf63179d95
VictorAriton <victorar@email.unc.edu>
2022-11-26 18:27:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import express from 'express';
-import {roll} from './lib/roll.js';
+import roll from './lib/roll.js';
 import parseArgs from 'minimist';
 
 const args = minimist(process.argv.slice(2));
