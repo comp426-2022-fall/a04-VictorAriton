53d2738ed1f51172bfd9e7b2a1d2b3f5b38f5f6a
VictorAriton <victorar@email.unc.edu>
2022-11-26 19:11:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ import express from "express"
 import roll from "./lib/roll.js"
 
 const app = express();
-
 const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
 
-app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-let port = 5000;
 
-if(args.port){
-    port = args.port;
-}
 
 app.get('/app/', (req, res) => {
     res.status(200);
@@ -23,26 +18,23 @@ app.get('/app/roll/', (req, res) => {
     res.send(roll(6, 2, 1));
 });
 
-
-
-
-app.post("/app/roll", (req, res) => {
-    const sides = parseInt(req.body.sides);
-    const dice = parseInt(req.body.dice);
-    const rolls = parseInt(req.body.rolls);
-    res.status(200).json(roll(sides,dice,rolls));
-  })
+app.post('/app/roll/', (req, res, next) => { //endpoint HTTP accept
+    let sides = parseInt(req.body.sides);
+    let dice = parseInt(req.body.dice);
+    let rolls = parseInt(req.body.rolls);
+    res.send(roll(sides, dice, rolls)).end();
+});
 
 app.get('/app/roll/:sides/', (req, res, next) => {
     let sides = parseInt(req.params.sides);
     res.send(roll(sides, 2, 1)).end();
 });
 
-app.get('/app/roll/:sides/:dice', (req, res) => {
-    const sides =  parseInt(req.params.sides);
-    const dice =  parseInt(req.params.dice);
-    res.status(200).json((roll(sides, dice,1)));
-  })
+app.get('/app/roll/:sides/:dice/', (req, res, next) => {
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    res.send(roll(sides, dice, 1)).end();
+});
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
     let sides = parseInt(req.params.sides);
